d8ac75d983b2af7b5bcc3408dc871a7b2270a4d5
Richard Neat <richard.neat@northcoders.com>
2024-10-07 15:48:14 +0100

    add starter repo

---
+++ TestingAPIS/Joke.cs
@@ -0,0 +1,10 @@
+﻿namespace TestingAPIS
+{
+    public class Joke
+    {
+        public string SetupLine { get; set; } = "";
+        public string PunchLine { get; set; } = "";
+        public string Category { get; set; } = "";
+        public bool IsFunny { get; set; }
+    }
+}
