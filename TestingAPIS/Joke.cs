f1f9d975b401ca6930a80d06b527970b605663f7
Rich Neat <151729655+nc-richard-neat@users.noreply.github.com>
2024-10-08 11:05:37 +0100

    Update Joke.cs

--- TestingAPIS/Joke.cs
+++ TestingAPIS/Joke.cs
@@ -2,6 +2,7 @@
 {
     public class Joke
     {
+        public int Id { get; set; }
         public string SetupLine { get; set; } = "";
         public string PunchLine { get; set; } = "";
         public string Category { get; set; } = "";
